ce5449e47c3ddb53ea3cab80b7bc31e95655710c
theiconnz <theiconnz@gmail.com>
2017-07-23 16:00:37 +1200

    0.0.1

---
+++ registration.php
@@ -0,0 +1,19 @@
+<?php 
+/**
+ * Nzpost (https://theicon.co.nz/)
+ *
+ * Module
+ *
+ * PHP version 7
+ *
+ * @category Module
+ * @package  Nzpost
+ * @author   Don Nuwinda Udugala<support@theicon.co.nz>
+ * @license  GPL https://theicon.co.nz
+ * @link     https://theicon.co.nz/magento2/nzpost
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Theiconnz_Nzpost',
+    __DIR__
+    );
